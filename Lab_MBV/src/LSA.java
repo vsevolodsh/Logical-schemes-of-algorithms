76ce3944772bcf941a9f74a6580de7c252f3759c
vsevolodsh <86848476+vsevolodsh@users.noreply.github.com>
2022-03-04 23:50:53 +0400

    Update LSA.java

--- Lab_MBV/src/LSA.java
+++ Lab_MBV/src/LSA.java
@@ -6,8 +6,11 @@ import java.util.Scanner;
 public class LSA {
 
     public static String[] Alphabet = "YnkX^vw123456789".split("");
+    public static String[] Nums = "123456789".split("");
     public String yArrray[];
-    public   String xArray[] = new String[255];
+    public String xArray[] = new String[255];
+    public String numXArray[] = new String[255];
+    public String numYArray[] = new String[255];
 
 
     public static String consoleImput() {
@@ -67,22 +70,52 @@ public class LSA {
         return checkDone;
     }
 
-    public  String[] getAllX(String entrance) {
+    public String[] getAllX(String entrance) {
 
         int j = 0;
         String[] newStrArr = entrance.split("");
         for (int i = 0; i < newStrArr.length; i++) {
             if (newStrArr[i].equals("X")) {
-                xArray[j] = newStrArr[i]+newStrArr[i+1];
+                xArray[j] = newStrArr[i];
                 j++;
             }
         }
         return xArray;
     }
 
-    public static String getCurrentSymbol(String entrance, int i) {
+    public String[] getAllNumAfterX(String entrance) {
+        int j = 0;
         String[] newStrArr = entrance.split("");
-        return newStrArr[i];
+        for (int i = 1; i < newStrArr.length; i++) {
+            for (int k = 0; k < Nums.length; k++) {
+                if (newStrArr[i].equals(Nums[k]) && newStrArr[i - 1].equals("X")) {
+                    numXArray[j] = newStrArr[i];
+                    j++;
+                }
+            }
+        }
+        return numXArray;
     }
+
+    public String[] getAllNumAfterY(String entrance) {
+        int j = 0;
+        String[] newStrArr = entrance.split("");
+        for (int i = 1; i < newStrArr.length; i++) {
+            for (int k = 0; k < Nums.length; k++) {
+                if (newStrArr[i - 1].equals("Y") && newStrArr[i].equals(Nums[k])) {
+                    numYArray[j] = newStrArr[i];
+                    j++;
+                }
+                if (newStrArr[i - 1].equals("Y") && newStrArr[i].equals("k"))
+                {
+                    numYArray[j] = newStrArr[i];
+                    j++;
+                }
+            }
+        }
+        return numYArray;
+    }
+
+  
 
 }
