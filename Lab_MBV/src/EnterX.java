63604e784704f95f9f1c6e5c1176420c5f77c2ae
Danichka13 <99400335+Danichka13@users.noreply.github.com>
2022-03-10 22:07:33 +0400

    Update EnterX.java

--- Lab_MBV/src/EnterX.java
+++ Lab_MBV/src/EnterX.java
@@ -1,64 +1,119 @@
-import java.io.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class LSA {
+public class EnterX {
+    public static String newStr = "";
 
-    public static String[] Alphabet = "YnkX^vw1234567890".split("");
+    public static ArrayList<String> createArrayOfString(String str) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        String[] arrStr = str.split("");
+        String temp = "";
+        int i = 0;
+        while (i != arrStr.length) {
+            temp = "";
 
-    public static String consoleInput() {
-        System.out.println("Введите ЛСА: ");
-        Scanner scn = new Scanner(System.in);
-        String entrance = scn.nextLine();
-        if (!checkEntrance(entrance)) {
-            System.out.println("\nПовторите ввод: ");
-            consoleInput();
-        }
-        return entrance;
-    }
-
-    public static String fileInput(String name) {
-        StringBuilder str = new StringBuilder();
-
-        try (FileReader reader = new FileReader(name)) {
-            FileInputStream fis = new FileInputStream(name);
-            int c;
-            char[] buff = new char[fis.available()];
-
-            while ((c = reader.read(buff)) > 0) {
-                if (c < buff.length + 1) {
-                    buff = Arrays.copyOf(buff, c);
-                    str.append(buff);
-                }
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        return str.toString();
-    }
-
-    private static boolean checkEntrance(String entrance) {
-        boolean checkDone = true;
-        String[] newStrArr = entrance.split("");
-        boolean[] checkArr = new boolean[newStrArr.length];
-
-        if (entrance.startsWith("Yn") && entrance.endsWith("Yk")) {
-            for (int i = 0; i < newStrArr.length; i++) {
-                for (int j = 0; j < Alphabet.length; j++) {
-
-                    if (newStrArr[i].equals(Alphabet[j])) {
-                        checkArr[i] = true;
+            if (arrStr[i].equals("Y")) {
+                int j = i;
+                temp += arrStr[j];
+                j++;
+                if (arrStr[j].equals("n") || arrStr[j].equals("k")) {
+                    temp += arrStr[j];
+                } else {
+                    while (Character.isDigit(arrStr[j].charAt(0))) {
+                        temp += arrStr[j];
+                        j++;
                     }
                 }
-                if (!checkArr[i]) {
-                    System.out.print("Символ номер " + (i + 1) + " неправильный; ");
-                    checkDone = false;
+                arrayList.add(temp);
+            }
+            if (arrStr[i].equals("X")) {
+                int j = i;
+                temp += arrStr[j];
+                j++;
+                while (Character.isDigit(arrStr[j].charAt(0))) {
+                    temp += arrStr[j];
+                    j++;
+                }
+                temp += arrStr[j];
+                j++;
+                while (Character.isDigit(arrStr[j].charAt(0))) {
+                    temp += arrStr[j];
+                    j++;
+                }
+                arrayList.add(temp);
+            }
+            if (arrStr[i].equals("v")) {
+                int j = i;
+                temp += arrStr[j];
+                j++;
+                while (Character.isDigit(arrStr[j].charAt(0))) {
+                    temp += arrStr[j];
+                    j++;
+                }
+                arrayList.add(temp);
+            }
+            if (arrStr[i].equals("W")) {
+                int j = i;
+                temp += arrStr[j] + arrStr[j + 1];
+                j += 2;
+                while (Character.isDigit(arrStr[j].charAt(0))) {
+                    temp += arrStr[j];
+                    j++;
+                }
+                arrayList.add(temp);
+            }
+            i++;
+        }
+        return arrayList;
+    }
+
+    public static String createString(String entrance, int index) {
+        ArrayList<String> etalonArr = createArrayOfString(entrance);
+        for (int i = index; i < etalonArr.size(); i++) {
+            index = i;
+            String str = etalonArr.get(i);
+            if (str.substring(0, 1).equals("Y")) {
+                if (str.equals("Yk")) {
+                    newStr += str;
+                    break;
+                } else {
+                    newStr += str;
+                    createString(entrance, index + 1);
+                    break;
                 }
             }
-        } else {
-            System.out.println("Проверьте правильность ввода начального и конечного оператора");
-            checkDone = false;
+            if (str.substring(0, 1).equals("X")) {
+                if (getX() == 0) {
+                    String temp = "v" + str.substring(str.indexOf("^") + 1);
+                    createString(entrance, etalonArr.indexOf(temp) + 1);
+                    break;
+                } else {
+                    createString(entrance, index + 1);
+                    break;
+                }
+            }
+            if (str.substring(0, 1).equals("W")) {
+                String temp = "v" + str.substring(str.indexOf("^") + 1);
+                createString(entrance, etalonArr.indexOf(temp) + 1);
+                break;
+            }
+            //i++;
         }
-        return checkDone;
+        return newStr;
+    }
+
+    public static String strChange(ArrayList<String> arr, int i) {
+        String str = "";
+        while (i < arr.size()) {
+            str += arr.get(i);
+            i++;
+        }
+        return str;
+    }
+
+    public static int getX() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите х:");
+        return scan.nextInt();
     }
 }
