d6b6431ef79a077a29d08d6108034561b2c266db
Danichka13 <99400335+Danichka13@users.noreply.github.com>
2022-03-10 22:06:57 +0400

    Update LSA.java

--- Lab_MBV/src/LSA.java
+++ Lab_MBV/src/LSA.java
@@ -1,25 +1,18 @@
-
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class LSA {
 
-    public static String[] Alphabet = "YnkX^vw123456789".split("");
-    public static String[] Nums = "123456789".split("");
-    public String yArrray[];
-    public String xArray[] = new String[255];
-    public String numXArray[] = new String[255];
-    public String numYArray[] = new String[255];
+    public static String[] Alphabet = "YnkX^vw1234567890".split("");
 
-
-    public static String consoleImput() {
+    public static String consoleInput() {
         System.out.println("Введите ЛСА: ");
         Scanner scn = new Scanner(System.in);
         String entrance = scn.nextLine();
         if (!checkEntrance(entrance)) {
             System.out.println("\nПовторите ввод: ");
-            consoleImput();
+            consoleInput();
         }
         return entrance;
     }
@@ -56,7 +49,6 @@ public class LSA {
                     if (newStrArr[i].equals(Alphabet[j])) {
                         checkArr[i] = true;
                     }
-
                 }
                 if (!checkArr[i]) {
                     System.out.print("Символ номер " + (i + 1) + " неправильный; ");
@@ -69,53 +61,4 @@ public class LSA {
         }
         return checkDone;
     }
-
-    public String[] getAllX(String entrance) {
-
-        int j = 0;
-        String[] newStrArr = entrance.split("");
-        for (int i = 0; i < newStrArr.length; i++) {
-            if (newStrArr[i].equals("X")) {
-                xArray[j] = newStrArr[i];
-                j++;
-            }
-        }
-        return xArray;
-    }
-
-    public String[] getAllNumAfterX(String entrance) {
-        int j = 0;
-        String[] newStrArr = entrance.split("");
-        for (int i = 1; i < newStrArr.length; i++) {
-            for (int k = 0; k < Nums.length; k++) {
-                if (newStrArr[i].equals(Nums[k]) && newStrArr[i - 1].equals("X")) {
-                    numXArray[j] = newStrArr[i];
-                    j++;
-                }
-            }
-        }
-        return numXArray;
-    }
-
-    public String[] getAllNumAfterY(String entrance) {
-        int j = 0;
-        String[] newStrArr = entrance.split("");
-        for (int i = 1; i < newStrArr.length; i++) {
-            for (int k = 0; k < Nums.length; k++) {
-                if (newStrArr[i - 1].equals("Y") && newStrArr[i].equals(Nums[k])) {
-                    numYArray[j] = newStrArr[i];
-                    j++;
-                }
-                if (newStrArr[i - 1].equals("Y") && newStrArr[i].equals("k"))
-                {
-                    numYArray[j] = newStrArr[i];
-                    j++;
-                }
-            }
-        }
-        return numYArray;
-    }
-
-  
-
 }
