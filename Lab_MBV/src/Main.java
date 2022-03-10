d145413e863d4fbe4dd899226788a95fdbe24ffd
Danichka13 <99400335+Danichka13@users.noreply.github.com>
2022-03-10 22:07:53 +0400

    Update Main.java

--- Lab_MBV/src/Main.java
+++ Lab_MBV/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
 
-    EnterX.inputAllX(LSA.consoleImput());
+        String lsa = "Ynv1X0^1v2X1^6X2^8v5Y5Y6W^9v6X2^7Y2Y3v3X3^5W^4v7Y1Y2v4Y5W^2v8Y1Y4W^3v9Yk";
+        System.out.println(EnterX.createArrayOfString(lsa));
+        System.out.println(EnterX.createString(lsa, 0));
     }
 }
