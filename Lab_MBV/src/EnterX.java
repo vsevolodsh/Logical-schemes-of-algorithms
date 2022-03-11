0d5c23d2c30453bb1c294d8a673f1437af5fce02
Danichka13 <99400335+Danichka13@users.noreply.github.com>
2022-03-11 22:02:38 +0400

    Update EnterX.java

--- Lab_MBV/src/EnterX.java
+++ Lab_MBV/src/EnterX.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EnterX {
-    public static String newStr = "";
+    public static String newStrConsX = "";
+    public static String newStrAllX = "";
+    public static int indexX = 0;
 
     public static ArrayList<String> createArrayOfString(String str) {
         ArrayList<String> arrayList = new ArrayList<>();
@@ -67,48 +70,147 @@ public class EnterX {
         return arrayList;
     }
 
-    public static String createString(String entrance, int index) {
+    public static String consecutiveX(String entrance, int index) {
         ArrayList<String> etalonArr = createArrayOfString(entrance);
         for (int i = index; i < etalonArr.size(); i++) {
             index = i;
             String str = etalonArr.get(i);
             if (str.substring(0, 1).equals("Y")) {
                 if (str.equals("Yk")) {
-                    newStr += str;
+                    newStrConsX += str;
                     break;
                 } else {
-                    newStr += str;
-                    createString(entrance, index + 1);
+                    newStrConsX += str;
+                    consecutiveX(entrance, index + 1);
                     break;
                 }
             }
+
             if (str.substring(0, 1).equals("X")) {
-                if (getX() == 0) {
+                int gX = getX();
+                if (gX == 0) {
                     String temp = "v" + str.substring(str.indexOf("^") + 1);
-                    createString(entrance, etalonArr.indexOf(temp) + 1);
+                    System.out.println("Промежуточный результат: " + newStrConsX);
+                    consecutiveX(entrance, etalonArr.indexOf(temp) + 1);
+                    break;
+                } else if (gX == 1) {
+                    System.out.println("Промежуточный результат: " + newStrConsX);
+                    consecutiveX(entrance, index + 1);
                     break;
                 } else {
-                    createString(entrance, index + 1);
+                    System.out.println("Неверное значение X! Повторите ввод!");
+                    consecutiveX(entrance, index);
                     break;
                 }
             }
+
             if (str.substring(0, 1).equals("W")) {
                 String temp = "v" + str.substring(str.indexOf("^") + 1);
-                createString(entrance, etalonArr.indexOf(temp) + 1);
+                consecutiveX(entrance, etalonArr.indexOf(temp) + 1);
                 break;
             }
-            //i++;
         }
-        return newStr;
+        return newStrConsX;
     }
 
-    public static String strChange(ArrayList<String> arr, int i) {
-        String str = "";
-        while (i < arr.size()) {
-            str += arr.get(i);
+    public static String allX(String entrance, int index, ArrayList<String> arrX) {
+        ArrayList<String> etalonArr = createArrayOfString(entrance);
+        for (int i = index; i < etalonArr.size(); i++) {
+            index = i;
+            String str = etalonArr.get(i);
+            if (str.substring(0, 1).equals("Y")) {
+                if (str.equals("Yk")) {
+                    newStrAllX += str;
+                    break;
+                } else {
+                    newStrAllX += str;
+                    allX(entrance, index + 1, arrX);
+                    break;
+                }
+            }
+
+            if (str.substring(0, 1).equals("X")) {
+                if (arrX.get(Integer.parseInt(getNum(str))).equals("0")) {
+                    String temp = "v" + str.substring(str.indexOf("^") + 1);
+                    allX(entrance, etalonArr.indexOf(temp) + 1, arrX);
+                    break;
+                } else {
+                    allX(entrance, index + 1, arrX);
+                    break;
+                }
+            }
+
+            if (str.substring(0, 1).equals("W")) {
+                String temp = "v" + str.substring(str.indexOf("^") + 1);
+                allX(entrance, etalonArr.indexOf(temp) + 1, arrX);
+                break;
+            }
+        }
+        return newStrAllX;
+    }
+
+    public static String allValueX(String entrance, int index, ArrayList<String> arrX) {
+        ArrayList<String> etalonArr = createArrayOfString(entrance);
+        for (int i = index; i < etalonArr.size(); i++) {
+            index = i;
+            String str = etalonArr.get(i);
+            if (str.substring(0, 1).equals("Y")) {
+                if (str.equals("Yk")) {
+                    newStrAllX += str;
+                    break;
+                } else {
+                    newStrAllX += str;
+                    allValueX(entrance, index + 1, arrX);
+                    break;
+                }
+            }
+
+            if (str.substring(0, 1).equals("X")) {
+                if (arrX.get(indexX).equals("0")) {
+                    indexX++;
+                    String temp = "v" + str.substring(str.indexOf("^") + 1);
+                    allValueX(entrance, etalonArr.indexOf(temp) + 1, arrX);
+                    break;
+                } else {
+                    indexX++;
+                    allValueX(entrance, index + 1, arrX);
+                    break;
+                }
+            }
+
+            if (str.substring(0, 1).equals("W")) {
+                String temp = "v" + str.substring(str.indexOf("^") + 1);
+                allValueX(entrance, etalonArr.indexOf(temp) + 1, arrX);
+                break;
+            }
+        }
+        return newStrAllX;
+    }
+
+    public static String getNum(String str) {
+        char[] ch = str.toCharArray();
+        int i = 1;
+        String num = "";
+        while (Character.isDigit(ch[i])) {
+            num += ch[i];
             i++;
         }
-        return str;
+        return num;
+    }
+
+    public static ArrayList<String> enterX() {
+        System.out.println("Введите все X: ");
+        Scanner scan = new Scanner(System.in);
+        String[] x = scan.nextLine().split("");
+        ArrayList<String> listX = new ArrayList<String>(Arrays.asList(x));
+        for (int i = 0; i < x.length; i++) {
+            if (!x[i].equals("0") && !x[i].equals("1")) {
+                System.out.println("Неверное значение X под номером " + (i + 1) + "\nПовторите ввод!");
+                listX = enterX();
+                break;
+            }
+        }
+        return listX;
     }
 
     public static int getX() {
