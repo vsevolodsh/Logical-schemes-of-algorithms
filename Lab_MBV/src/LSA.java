bb4c43a59d6401cc6823670a75d752f67c5bce5d
vsevolodsh <86848476+vsevolodsh@users.noreply.github.com>
2022-03-11 21:30:10 +0400

    Update LSA.java

--- Lab_MBV/src/LSA.java
+++ Lab_MBV/src/LSA.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class LSA {
 
     public static String[] Alphabet = "YnkX^vw1234567890".split("");
+    public static String[] Nums = "1234567890".split("");
 
     public static String consoleInput() {
         System.out.println("Введите ЛСА: ");
@@ -12,29 +13,34 @@ public class LSA {
         String entrance = scn.nextLine();
         if (!checkEntrance(entrance)) {
             System.out.println("\nПовторите ввод: ");
-            consoleInput();
+            entrance = consoleInput();
         }
         return entrance;
     }
 
-    public static String fileInput(String name) {
-        StringBuilder str = new StringBuilder();
+    public static String fileInput(String entrance) {
+        StringBuilder result = new StringBuilder();
 
-        try (FileReader reader = new FileReader(name)) {
-            FileInputStream fis = new FileInputStream(name);
+        try (FileReader reader = new FileReader(entrance)) {
+            FileInputStream fis = new FileInputStream(entrance);
             int c;
             char[] buff = new char[fis.available()];
 
             while ((c = reader.read(buff)) > 0) {
                 if (c < buff.length + 1) {
                     buff = Arrays.copyOf(buff, c);
-                    str.append(buff);
+                    result.append(buff);
                 }
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        return str.toString();
+
+        if (!checkEntrance(result.toString())) {
+            System.out.println("Проверьте содержимое файла и повторите попытку снова");
+            System.exit(0);
+        }
+        return result.toString();
     }
 
     private static boolean checkEntrance(String entrance) {
@@ -49,6 +55,7 @@ public class LSA {
                     if (newStrArr[i].equals(Alphabet[j])) {
                         checkArr[i] = true;
                     }
+
                 }
                 if (!checkArr[i]) {
                     System.out.print("Символ номер " + (i + 1) + " неправильный; ");
@@ -59,6 +66,44 @@ public class LSA {
             System.out.println("Проверьте правильность ввода начального и конечного оператора");
             checkDone = false;
         }
+
+
+        for (int i = 3; i < newStrArr.length - 1; i++) {
+            char ch = newStrArr[i].charAt(0);
+            if (newStrArr[i - 1].equals("Y")) {
+
+                if (!(Character.isDigit(ch))) {
+                    System.out.println("После Y должна ставиться какая-то цифра, проверьте элемент " + i);
+                    checkDone = false;
+                }
+
+            }
+            if (newStrArr[i - 1].equals("X")) {
+
+                if (!(Character.isDigit(ch))) {
+                    System.out.println("После X должна ставиться какая-то цифра, проверьте элемент " + i);
+                    checkDone = false;
+                }
+            }
+            if (newStrArr[i - 1].equals("v")) {
+
+                if (!(Character.isDigit(ch))) {
+                    System.out.println("После нижней стрелки должна ставиться какая-то цифра, проверьте элемент " + i);
+                    checkDone = false;
+                }
+            }
+            if (newStrArr[i - 1].equals("w")) {
+                if ((!newStrArr[i].equals("^"))) {
+                    System.out.println("После безусловного перехода w должна ставиться стрелка вверх, проверьте элемент " + i);
+                }
+                if (!(Character.isDigit(newStrArr[i + 1].charAt(0)))) {
+                    System.out.println("После стрелки вверх должна ставиться какая-то цифра, проверьте элемент " + (i + 1));
+                    checkDone = false;
+                }
+            }
+
+        }
         return checkDone;
     }
+
 }
